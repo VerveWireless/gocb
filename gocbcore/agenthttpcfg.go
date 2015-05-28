d65c5402a57993aff6a68241d7d41e5fe81703cd
Skye Berghel <skye@vervemobile.com>
2015-05-28 16:50:17 -0700

    buckets can now be closed

--- gocbcore/agenthttpcfg.go
+++ gocbcore/agenthttpcfg.go
@@ -9,6 +9,11 @@ import (
 	"time"
 )
 
+const (
+	maxConnPeriod = 10 * time.Second
+	waitPeriod    = 20 * time.Second
+)
+
 type configStreamBlock struct {
 	Bytes []byte
 }
@@ -28,8 +33,6 @@ func hostnameFromUri(uri string) string {
 }
 
 func (c *Agent) httpLooper(firstCfgFn func(*cfgBucket, error)) {
-	waitPeriod := 20 * time.Second
-	maxConnPeriod := 10 * time.Second
 	var iterNum uint64 = 1
 	iterSawConfig := false
 	seenNodes := make(map[string]uint64)
@@ -37,112 +40,117 @@ func (c *Agent) httpLooper(firstCfgFn func(*cfgBucket, error)) {
 
 	logDebugf("HTTP Looper starting.")
 	for {
-		routingInfo := c.routingInfo.get()
+		select {
+		case <-c.stop:
+			return
+		default:
+			routingInfo := c.routingInfo.get()
 
-		var pickedSrv string
-		for _, srv := range routingInfo.mgmtEpList {
-			if seenNodes[srv] >= iterNum {
-				continue
-			}
-			pickedSrv = srv
-			break
-		}
-
-		if pickedSrv == "" {
-			logDebugf("Pick Failed.")
-			// All servers have been visited during this iteration
-			if isFirstTry {
-				logDebugf("Could not find any alive http hosts.")
-				firstCfgFn(nil, &agentError{"Failed to connect to all specified hosts."})
-				return
-			} else {
-				if !iterSawConfig {
-					logDebugf("Looper waiting...")
-					// Wait for a period before trying again if there was a problem...
-					<-time.After(waitPeriod)
+			var pickedSrv string
+			for _, srv := range routingInfo.mgmtEpList {
+				if seenNodes[srv] >= iterNum {
+					continue
 				}
-				logDebugf("Looping again.")
-				// Go to next iteration and try all servers again
-				iterNum++
-				iterSawConfig = false
+				pickedSrv = srv
+				break
+			}
+
+			if pickedSrv == "" {
+				logDebugf("Pick Failed.")
+				// All servers have been visited during this iteration
+				if isFirstTry {
+					logDebugf("Could not find any alive http hosts.")
+					firstCfgFn(nil, &agentError{"Failed to connect to all specified hosts."})
+					return
+				} else {
+					if !iterSawConfig {
+						logDebugf("Looper waiting...")
+						// Wait for a period before trying again if there was a problem...
+						<-time.After(waitPeriod)
+					}
+					logDebugf("Looping again.")
+					// Go to next iteration and try all servers again
+					iterNum++
+					iterSawConfig = false
+					continue
+				}
+			}
+
+			logDebugf("Http Picked: %s.", pickedSrv)
+
+			seenNodes[pickedSrv] = iterNum
+
+			hostname := hostnameFromUri(pickedSrv)
+
+			logDebugf("HTTP Hostname: %s.", pickedSrv)
+
+			// HTTP request time!
+			uri := fmt.Sprintf("%s/pools/default/bucketsStreaming/%s", pickedSrv, c.bucket)
+
+			req, err := http.NewRequest("GET", uri, nil)
+			if err != nil {
+				logDebugf("Failed to build HTTP config request. %v", err)
 				continue
 			}
-		}
 
-		logDebugf("Http Picked: %s.", pickedSrv)
+			req.SetBasicAuth(c.bucket, c.password)
 
-		seenNodes[pickedSrv] = iterNum
-
-		hostname := hostnameFromUri(pickedSrv)
-
-		logDebugf("HTTP Hostname: %s.", pickedSrv)
-
-		// HTTP request time!
-		uri := fmt.Sprintf("%s/pools/default/bucketsStreaming/%s", pickedSrv, c.bucket)
-
-		req, err := http.NewRequest("GET", uri, nil)
-		if err != nil {
-			logDebugf("Failed to build HTTP config request. %v", err)
-			continue
-		}
-
-		req.SetBasicAuth(c.bucket, c.password)
-
-		resp, err := c.httpCli.Do(req)
-		if err != nil {
-			logDebugf("Failed to connect to host. %v", err)
-			continue
-		}
-
-		if resp.StatusCode != 200 {
-			if resp.StatusCode == 401 {
-				logDebugf("Failed to connect to host, bad auth.")
-				firstCfgFn(nil, &memdError{StatusAuthError})
-				return
-			}
-			logDebugf("Failed to connect to host, unexpected status code: %v.", resp.StatusCode)
-			continue
-		}
-
-		logDebugf("Connected.")
-
-		// Autodisconnect eventually
-		go func() {
-			<-time.After(maxConnPeriod)
-			logDebugf("Auto DC!")
-			resp.Body.Close()
-		}()
-
-		dec := json.NewDecoder(resp.Body)
-		configBlock := new(configStreamBlock)
-		for {
-			err := dec.Decode(configBlock)
+			resp, err := c.httpCli.Do(req)
 			if err != nil {
+				logDebugf("Failed to connect to host. %v", err)
+				continue
+			}
+
+			if resp.StatusCode != 200 {
+				if resp.StatusCode == 401 {
+					logDebugf("Failed to connect to host, bad auth.")
+					firstCfgFn(nil, &memdError{StatusAuthError})
+					return
+				}
+				logDebugf("Failed to connect to host, unexpected status code: %v.", resp.StatusCode)
+				continue
+			}
+
+			logDebugf("Connected.")
+
+			// Autodisconnect eventually
+			go func() {
+				<-time.After(maxConnPeriod)
+				logDebugf("Auto DC!")
 				resp.Body.Close()
-				break
+			}()
+
+			dec := json.NewDecoder(resp.Body)
+			configBlock := new(configStreamBlock)
+			for {
+				err := dec.Decode(configBlock)
+				if err != nil {
+					resp.Body.Close()
+					break
+				}
+
+				logDebugf("Got Block.")
+
+				bkCfg, err := parseConfig(configBlock.Bytes, hostname)
+				if err != nil {
+					resp.Body.Close()
+					break
+				}
+
+				logDebugf("Got Config.")
+
+				iterSawConfig = true
+				if isFirstTry {
+					logDebugf("HTTP Config Init")
+					firstCfgFn(bkCfg, nil)
+					isFirstTry = false
+				} else {
+					logDebugf("HTTP Config Update")
+					c.updateConfig(bkCfg)
+				}
 			}
 
-			logDebugf("Got Block.")
-
-			bkCfg, err := parseConfig(configBlock.Bytes, hostname)
-			if err != nil {
-				resp.Body.Close()
-				break
-			}
-
-			logDebugf("Got Config.")
-
-			iterSawConfig = true
-			if isFirstTry {
-				logDebugf("HTTP Config Init")
-				firstCfgFn(bkCfg, nil)
-				isFirstTry = false
-			} else {
-				logDebugf("HTTP Config Update")
-				c.updateConfig(bkCfg)
-			}
+			logDebugf("HTTP, Setting %s to iter %d", pickedSrv, iterNum)
 		}
-
-		logDebugf("HTTP, Setting %s to iter %d", pickedSrv, iterNum)
 	}
 }
